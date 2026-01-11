df8a35666df3e776dd5fb60be8cdf7f9649319b8
Mykhailo09Isariuk <isariuk.mykhailo@gmail.com>
2026-01-11 12:39:51 +0200

    This Project Is Completed

---
+++ main.py
@@ -0,0 +1,29 @@
+import math
+
+num_1 = float(input("Enter first number >>>   "))
+num_2  = float(input("Enter second number >>>  "))
+action = str(input("Enter action >>>  "))
+
+if action == '+':
+    answ = num_1 + num_2
+    print(f'Answer: {answ}')
+
+elif action == '-':
+    answ = num_1 - num_2
+    print(f'Answer: {answ}')
+
+elif action == '*':
+    answ = num_1 * num_2
+    print(f'Answer: {answ}')
+
+elif action == '/':
+    answ = num_1 / num_2
+    print(f'Answer: {answ}')
+
+elif action == '%':
+    answ = num_1 % num_2
+    print(f'Answer: {answ}')
+
+elif action == '//':
+    answ = num_1 // num_2
+    print(f'Answer: {answ}')
